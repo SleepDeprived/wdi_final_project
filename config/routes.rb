34471b2cac3a7a4cb346fe0937be16a6c56743f8
James Davis <jamesmdavis5@gmail.com>
2013-12-12 17:58:30 -0500

    working on github oauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ FinalProject::Application.routes.draw do
   root to: 'welcome#index'
 
   match '/auth/:provider/callback' => 'sessions#create'
+  match '/signout' => 'sessions#destroy', :as => :signout
 
   get "welcome/index"
 
