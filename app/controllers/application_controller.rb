3386c70df62da5dce26b89e2c038f0fb41c5afba
James Davis <jamesmdavis5@gmail.com>
2013-12-12 18:28:50 -0500

    fixing merge conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def redirect
   	unless @logged_in
   		redirect_to root_path
   	end
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :github_login, :last_name, :oauth_expires_at, :oauth_token, :proider, :uid
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.github_login = auth["login"]
+    end
+  end
+
 end
