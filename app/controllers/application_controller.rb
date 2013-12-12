bf42dbf4a02bafa25d7a3e530934e35ac05f090b
SleepDeprived <metzgert@gmail.com>
2013-12-12 18:15:12 -0500

    commented out before filter to open routes for development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :redirect
-
   def redirect
   	unless @logged_in
   		redirect_to root_path
   	end
   end
+  
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-	before_filter :redirect, except: :index
+	# before_filter :redirect, except: :index
 
   def index
   end
