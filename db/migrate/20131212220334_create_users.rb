3702a3454b2d77098d23afbba0d19a85ddda8080
SleepDeprived <metzgert@gmail.com>
2013-12-13 17:05:28 -0500

    user migration file adn model changes for fields captured when new user created

--- db/migrate/20131212220334_create_users.rb
+++ db/migrate/20131212220334_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :github_login
+      t.string :github_username
       t.string :uid
       t.string :provider
+      t.string :name
       t.string :email
+      t.text :github_image
       t.string :oauth_token
       t.datetime :oauth_expires_at
 
