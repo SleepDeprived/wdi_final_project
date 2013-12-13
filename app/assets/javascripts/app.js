eb0b59c79459000d69e9db13ce966e38cc16b15c
SleepDeprived <metzgert@gmail.com>
2013-12-13 00:26:37 -0500

    added timer elements to dashboard, began working on jasmine tests and implementing timer as HTML5 browser alert

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
  
   App.router = new App.Router();
+
+  // this should be moved to the appropriate view
+  // currently this code does not work
+  $('#start_timer_button').on('click', function() {
+	  if (window.webkitNotifications.checkPermission() == 0) { // 0 is PERMISSION_ALLOWED
+	    // function defined in step 2
+	    console.log("button works");
+	    window.webkitNotifications.createNotification(
+	        'icon.png', 'Notification Title', 'Notification content...');
+	  } else {
+	    window.webkitNotifications.requestPermission();
+	  }
+	}, false);
+
+
+
  
 });
---
+++ spec/javascripts/features/timer_tests.js
@@ -0,0 +1,14 @@
+describe('the timer', function(){
+
+	var $timer_toggle = $("#timer_toggle")
+	var $time = $("#time")
+	var $start_timer_button = $("#start_timer_button")
+
+	
+
+
+
+	
+}]);
+
+// $timer_toggle.is(':checked')
