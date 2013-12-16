d80cbf7745096d427a6f8c4f7f9173902f6e658e
SleepDeprived <metzgert@gmail.com>
2013-12-16 12:48:54 -0500

    updated application manifest and added backbone routes for dashboard, currently commented out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,5 +21,9 @@
 //= require setup
 //= require routes
 //= require_tree ./templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./features
 //
 //= require app
--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -5,7 +5,38 @@ App.Router = Backbone.Router.extend({
   initialize: function(){
     Backbone.history.start();
   },
-  index: function(){
-    console.log('index');
-  }
+  
+  // // summary graph on the dashboard when it loads
+  // dashboard: function(){
+  //   console.log('dashboard');
+  //   // summary will provide an overview of commits 
+  //   // initial version will be for a week
+  //   // Creates the collection to be passed into the view
+  //   var summary = new Project.Collections.Summaries();
+  //   // Creates the view that will be appended to the page
+  //   new Project.Views.SummariesView({collection: summary})
+  // }
+
 });
+
+
+
+// // code from in class exercise using as a template... to be deleted
+// routes: {
+//     ""             : "index",
+//     "exercises"          : "new",
+//     "random" : "random"
+//   },
+
+//   index: function(){
+//     console.log("index");
+//     exercises = new Exercises();
+//     exercisesView = new ExercisesView({collection: exercises});
+//   },
+//   new: function(){
+//     console.log('new');
+//     newForm = new ExerciseForm();
+//   },
+//   random: function() {
+//     randomView = new RandomView();
+//   }
