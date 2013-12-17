475c1a1844adca07eac0844e85432ea49dcf26f3
SleepDeprived <metzgert@gmail.com>
2013-12-17 10:06:56 -0500

    new.js in locations renamed to main.js

---
+++ app/assets/javascripts/location_map/views/main.js
@@ -0,0 +1,62 @@
+App.Views.Main = Backbone.View.extend({
+  el: "#side_nav",
+
+  events: {
+    "click #add_location"  : "showForm"
+  },
+
+  initialize: function(){
+    console.log("initialized main.js");
+    this.locationForm = new App.Views.LocationForm({model: this.model});
+  },
+
+  showForm: function(e){
+    console.log("show form");
+    this.locationForm.show();
+  }
+});
+
+App.Views.LocationForm = Backbone.View.extend({
+
+  el: "#location_form",
+
+  events: {
+    "submit form" : "createLocation"
+  },
+
+  initialize: function(){
+    this.name = $('#location_name');
+    this.address = $('#location_address');
+    this.address_detail = $('#location_address_detail');
+    this.city = $('#location_city');
+    this.state = $('#location_state');
+    this.zipcode = $('#location_zipcode');
+    this.description = $('#location_description');
+    this.listenTo(this.model, "invalid", this.displayErrors);
+  },
+  show: function(){
+    this.$el.show();
+  },
+  createLocation: function(e){
+    e.preventDefault();
+    console.log("form submitted");
+    this.model.set( this.getAttributes() );
+    if (this.model.isValid()){
+      this.model.save();
+    }
+  },
+  getAttributes: function(){
+    return {
+      name: this.name.val(),
+      address: this.address.val(),
+      address_detail: this.address_detail.val(),
+      city: this.city.val(),
+      state: this.state.val(),
+      zipcode: this.zipcode.val(),
+      description: this.description.val()
+    }
+  },
+  displayErrors: function(){
+    $("#errors").text(this.model.validationError);
+  }
+});
