b6ea18334745772a8458cdd1725581deb5c1c45d
James Davis <jamesmdavis5@gmail.com>
2013-12-18 16:18:28 -0500

    writing some jasmine tests for map

---
+++ spec/javascripts/features/map_spec.js
@@ -0,0 +1,23 @@
+describe("a map", function() {
+
+  var map;
+
+  beforeEach(function() {
+    map = new Map();
+    loc1 = { name: "GA West" };
+    loc2 = { name: "GA East" }
+  });
+
+  it("can get a location", function() {
+    expect(map.getLocation()).toBe(true);
+  });
+
+  it("can get multiple locations", function() {
+    expect(map.getLocations()).toBeGreaterThan(1);
+  });
+
+  it("has a pin", function() {
+    expect(map.addPin(loc1)).toBe(true);
+  });
+
+})
