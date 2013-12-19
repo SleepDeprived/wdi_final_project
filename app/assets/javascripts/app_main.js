08ec9ea97ed795b4e74bd05a76c6fe8ed895c3b2
James Davis <jamesmdavis5@gmail.com>
2013-12-18 23:19:43 -0500

    fixed add locations link on dash page

--- app/assets/javascripts/app_main.js
+++ app/assets/javascripts/app_main.js
@@ -26,5 +26,4 @@ $(document).ready(function(){
 	// $('#dashboard').on("click", )
 		// this is to target the dashboard link in the nav bar so that it will append or show divs that will trigger the different backbone views for the different charts
 
-});
 
