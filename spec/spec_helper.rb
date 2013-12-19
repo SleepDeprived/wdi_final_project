891227436d98e175bb54eee809b9ad24d1e76ff1
James Davis <jamesmdavis5@gmail.com>
2013-12-19 11:01:47 -0500

    added coveralls files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
+# must occur before any of your application code is required
+require 'coveralls'
+Coveralls.wear_merged!
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
