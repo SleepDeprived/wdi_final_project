4b66bbdb19072b50ad29c22a7209d84cd914a5c1
SleepDeprived <metzgert@gmail.com>
2013-12-18 23:06:57 -0500

    updated styling on nav bar and timer

--- app/assets/javascripts/app_main.js
+++ app/assets/javascripts/app_main.js
@@ -1,1 +1,7 @@
 // Put all the JS for the sidebar interactivity here
+$(document).ready(function(){
+
+	// $('#dashboard').on("click", )
+		// this is to target the dashboard link in the nav bar so that it will append or show divs that will trigger the different backbone views for the different charts
+
+});
