09d81a88b5a27f7b8066a6da37a2e5da12ae5526
James Davis <jamesmdavis5@gmail.com>
2013-12-18 23:13:39 -0500

    fixed add location link on dashboard page

--- app/assets/javascripts/app_main.js
+++ app/assets/javascripts/app_main.js
@@ -1,1 +1,23 @@
 // Put all the JS for the sidebar interactivity here
+$(document).ready(function(){
+  // When you click on the "Add Location" link in the side nav
+  // it will toggle the form to the create a new location
+  $("#add_location").on("click", function(e) {
+    e.preventDefault();
+    $("#location_form").toggle();
+    $("#add_location").toggleClass("menu-open");
+  });
+
+  $("#location_form").mouseup(function() {
+    return false;
+  });
+
+
+  // when you click again anywhere else, it will hide the form
+  $(document).mouseup(function(e) {
+    if( $(e.target).parent("#add_location").length==0 ) {
+      $("#add_location").removeClass("menu-open");
+      $("#location_form").hide();
+    }
+  });
+});
--- app/assets/javascripts/location_map/app.js
+++ app/assets/javascripts/location_map/app.js
@@ -2,36 +2,15 @@ $(document).ready(function(){
 
   App.router = new App.Router();
 
-  // When you click on the "Add Location" link in the side nav
-  // it will toggle the form to the create a new location
-  $("#add_location").on("click", function(e) {
-    e.preventDefault();
-    $("#location_form").toggle();
-    $("#add_location").toggleClass("menu-open");
-  });
-
-  $("#location_form").mouseup(function() {
-    return false;
-  });
-
-
-  // when you click again anywhere else, it will hide the form
-  $(document).mouseup(function(e) {
-    if( $(e.target).parent("#add_location").length==0 ) {
-      $("#add_location").removeClass("menu-open");
-      $("#location_form").hide();
-    }
-  });
-
   // ================================================================
 // This function makes flash notices fade in and fade out
 // ================================================================
 
-$(function() {
+
   $('#flash_notice').delay(500).fadeIn('normal', function() {
     $(this).delay(2000).fadeOut();
   });
-});
+
 
 });
 
